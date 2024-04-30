2c0e3f06f0f1fc2e2371c0374be4bd9cf3d55fe0
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-04-30 18:20:48 +0700

    player controller: player lompat

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,33 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D compPlayerBody;
+    [SerializeField] float maxMovementSpeed;
+    [SerializeField] float maxJumpPower;
+    [SerializeField] float maxFallSpeed;
 
     // Fungsi awal
     void Start()
     {
-        
+
     }
 
     // Update dengan sync fisika
-    void FixedUpdate()
+    void Update()
     {
-        // Gravitasi
-        compPlayerBody.velocity += Time.deltaTime * Physics2D.gravity;
-
+        // Gerak kanan kiri
         float direction = Input.GetAxis("Horizontal");
+        compPlayerBody.velocity = new Vector2(direction * maxMovementSpeed, compPlayerBody.velocity.y);
+
+        // Lompat
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            compPlayerBody.velocity = new Vector2(compPlayerBody.velocity.x, maxJumpPower);
+        }
+
+        // Batas kecepatan jatuh
+        compPlayerBody.velocity = new Vector2(compPlayerBody.velocity.x, Mathf.Max(-maxFallSpeed, compPlayerBody.velocity.y));
     }
 }
