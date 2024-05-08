c58c1d5e0cd66b4c3ca4b02cf7460c5f523ccc61
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-05-08 20:02:37 +0700

    player controller: detect tanah

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Collider
     [SerializeField] Rigidbody2D compPlayerBody;
+    [SerializeField] BoxCollider2D compGroundDetector;
+    [SerializeField] BoxCollider2D compWallDetector;
+
+    // Player
     [SerializeField] float maxMovementSpeed;
     [SerializeField] float maxJumpPower;
     [SerializeField] float maxFallSpeed;
+
+    // Status player
+    bool isOnGround;
+    bool isOnWall;
+
 
     // Fungsi awal
     void Start()
@@ -22,12 +32,30 @@ public class Player : MonoBehaviour
         compPlayerBody.velocity = new Vector2(direction * maxMovementSpeed, compPlayerBody.velocity.y);
 
         // Lompat
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && isOnGround)
         {
             compPlayerBody.velocity = new Vector2(compPlayerBody.velocity.x, maxJumpPower);
         }
 
         // Batas kecepatan jatuh
         compPlayerBody.velocity = new Vector2(compPlayerBody.velocity.x, Mathf.Max(-maxFallSpeed, compPlayerBody.velocity.y));
+    }
+
+    void FixedUpdate()
+    {
+        WallCheck();
+        GroundCheck();
+    }
+
+    // Deteksi tanah
+    void GroundCheck()
+    {
+        Collider2D collider = Physics2D.OverlapArea(compGroundDetector.bounds.min, compGroundDetector.bounds.max);
+        isOnGround = collider.gameObject.CompareTag("Ground");
+    }
+
+    void WallCheck()
+    {
+
     }
 }
