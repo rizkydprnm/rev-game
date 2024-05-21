8a0ad71892a1a653c4124e7afae8f47f69b9ec03
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-05-21 21:31:41 +0700

    implementasi wall jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,19 @@ public class Player : MonoBehaviour
     // Collider
     [SerializeField] Rigidbody2D playerBody;
     [SerializeField] Transform groundDetector;
-    [SerializeField] Transform leftWallDetector;
-    [SerializeField] Transform rightWallDetector;
+    [SerializeField] Transform wallDetector;
 
     // Player
     [SerializeField] float maxMovementSpeed;
     [SerializeField] float maxJumpPower;
     [SerializeField] float maxFallSpeed;
+    [SerializeField] float wallSlideSpeed;
+    [SerializeField] float wallJumpKnockback;
 
     // Status player
-    bool isOnGround, isOnWallLeft, isOnWallRight;
     bool canExtraJump;
+    bool canWallJump;
+    bool isWallJumping;
 
     // Fungsi awal
     void Start()
@@ -28,35 +30,54 @@ public class Player : MonoBehaviour
     {
         // Gerak kanan kiri
         float direction = Input.GetAxis("Horizontal");
-        playerBody.velocity = new Vector2(direction * maxMovementSpeed, playerBody.velocity.y);
+        if (!isWallJumping && direction != 0)
+            playerBody.velocity = new Vector2(direction * maxMovementSpeed, playerBody.velocity.y);
 
         // Lompat
         if (Input.GetButtonDown("Jump"))
         {
-            if (isOnGround || canExtraJump)
+            if (IsOnGround() || canExtraJump)
                 playerBody.velocity = new Vector2(playerBody.velocity.x, maxJumpPower);
-                if (canExtraJump) canExtraJump = false;
+            if (canExtraJump) canExtraJump = false;
+
+            // Wall jump
+            if (canWallJump)
+            {
+                isWallJumping = true;
+                if (direction < 0f) playerBody.velocity = new Vector2(wallJumpKnockback, maxJumpPower);
+                else if (direction > 0f) playerBody.velocity = new Vector2(-wallJumpKnockback, maxJumpPower);
+                Invoke(nameof(StopWallJump), 0.5f);
+            }
         }
 
-        // Batas kecepatan jatuh
-        playerBody.velocity = new Vector2(playerBody.velocity.x, Mathf.Max(-maxFallSpeed, playerBody.velocity.y));
+
+        // Bisa wall jump?
+        if (IsOnWall() && !IsOnGround() && direction != 0f)
+        {
+            canWallJump = true;
+            playerBody.velocity = new Vector2(playerBody.velocity.x, Mathf.Max(-wallSlideSpeed, playerBody.velocity.y));
+        }
+        else
+        {
+            canWallJump = false;
+            // Batas kecepatan jatuh
+            playerBody.velocity = new Vector2(playerBody.velocity.x, Mathf.Max(-maxFallSpeed, playerBody.velocity.y));
+        }
     }
 
-    void FixedUpdate()
+    bool IsOnGround()
     {
-        GroundCheck();
-        WallCheck();
+        return Physics2D.CircleCastAll(groundDetector.position, 0.5f, Vector2.down, 0.01f).Length > 1;
     }
 
-    void GroundCheck()
+    bool IsOnWall()
     {
-        isOnGround = Physics2D.CircleCastAll(groundDetector.position, 0.5f, Vector2.down, 0.01f).Length > 1;
-        if (isOnGround) canExtraJump = true;
+        return Physics2D.OverlapBoxAll(wallDetector.position, new Vector2(1.02f, 0.5f), 0).Length > 1;
     }
 
-    void WallCheck()
+    void StopWallJump()
     {
-
+        isWallJumping = false;
     }
 }
 
