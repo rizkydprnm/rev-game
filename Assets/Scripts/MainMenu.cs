124e73ef326fe007c3bebaea3e088535adec41e0
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-06-14 20:12:32 +0700

    tambah fungsionalitas main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+    }
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
