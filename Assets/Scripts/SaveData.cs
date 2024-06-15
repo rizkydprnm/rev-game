3f0290a3b2996d721d341602bba5903afe8739bf
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-06-15 22:03:00 +0700

    default value untuk save system

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SaveData : MonoBehaviour
 {
-    public int currentStage;
-    public Vector2 currentCheck;
-    public float currentTime;
+    public int currentStage = 1;
+    public Vector2 currentCheck = Vector2.zero;
+    public float currentTime = 0f;
 }
