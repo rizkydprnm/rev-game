e5254cb76aeee20242b37e1a17d5f4fea962d8be
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-06-18 21:17:47 +0700

    tambah prefab checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField] Animator anim;
+    bool passed;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.CompareTag("Player"))
+        if (collider.gameObject.CompareTag("Player") && !passed)
         {
+            passed = true;
+            anim.Play("AnimCheckRise");
             collider.gameObject.GetComponent<Player>().SetCheckpoint(transform.position);
         }
     }
