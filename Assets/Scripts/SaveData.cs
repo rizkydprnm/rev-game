a852ba353608e4a349b48c821a4201f556adfc5a
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-06-20 15:14:15 +0700

    tambah lives di save data

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class SaveData
 {
-    public int currentStage = 1;
+    public int stage = 1;
     public Vector2 currentCheck = Vector2.zero;
     public float currentTime = 0f;
+    public int lives = 5;
 }
